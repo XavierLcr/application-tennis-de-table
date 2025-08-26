525b9a887723b719333ca0b40ea07088b4927a3f
Xavier <xaruolac@gmail.com>
2025-08-26 16:16:01 +0200

    Simplification.

--- fonctions_utiles.py
+++ fonctions_utiles.py
@@ -1,13 +1,12 @@
 def trier_cles_par_params(dictionnaire, *parametres):
-    # Créer une liste de tuples : (clé, valeur_param1, valeur_param2, ...)
-    cles_et_valeurs = [
-        (cle, *(sous_dict[param] for param in parametres))
-        for cle, sous_dict in dictionnaire.items()
-    ]
 
     # Trier les tuples selon tous les paramètres donnés, dans l'ordre décroissant
     cles_et_valeurs_triees = sorted(
-        cles_et_valeurs,
+        # Créer la liste de tuples : (clé, valeur_param1, valeur_param2, ...)
+        [
+            (cle, *(sous_dict[param] for param in parametres))
+            for cle, sous_dict in dictionnaire.items()
+        ],
         key=lambda x: tuple(-v for v in x[1:]),
     )
 
